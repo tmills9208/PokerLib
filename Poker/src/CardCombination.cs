ae943bddf08304f9fc077b01acc275d108286859
Tyler Mills <tmills9208@gmail.com>
2023-05-21 07:21:06 -0400

    Adding Combination Handler & Tests

--- Poker/src/CardCombination.cs
+++ Poker/src/CardCombination.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.XPath;
 
 namespace Poker
 {
@@ -19,72 +20,127 @@ namespace Poker
         FourOfAKind,
         StraightFlush
     }
-
     public class HandCombination
     {
         public readonly CardCombo Combo;
-        public readonly PokerCardRank HighestRank;
+        public readonly PokerCardRank[] Ranks;
+        public readonly PokerCardSuit[] Suits;
 
-        public HandCombination(CardCombo combo, PokerCardRank highestRank)
+        #region Constructors
+        public HandCombination(CardCombo combo, PokerCardRank rank, PokerCardSuit suit)
         {
             Combo = combo;
-            HighestRank = highestRank;
+            Ranks = new PokerCardRank[] { rank };
+            Suits = new PokerCardSuit[] { suit };
         }
+        public HandCombination(CardCombo combo, PokerCardRank[] ranks, PokerCardSuit suit)
+        {
+            Combo = combo;
+            Ranks = ranks;
+            Suits = new PokerCardSuit[] { suit };
+        }
+        public HandCombination(CardCombo combo, PokerCardRank rank, PokerCardSuit[] suits)
+        {
+            Combo = combo;
+            Ranks = new PokerCardRank[] { rank };
+            Suits = suits;
+        }
+        public HandCombination(CardCombo combo, PokerCardRank[] ranks, PokerCardSuit[] suits)
+        {
+            Combo = combo;
+            Ranks = ranks;
+            Suits = suits;
+        }
+        #endregion
     }
 
-    public static class CardCombinationHandler
+    public static class HandCombinationHandler
     {
         public static HandCombination CalculateHighestCombination(List<StandardCard> cards)
         {
             if (cards.Count < 5) throw new ArgumentException("There must be at least 5 cards to calculate from");
 
-            PokerCardSuit similarSuit = PokerCardSuit.Clubs;
-            PokerCardRank similarRank = PokerCardRank.Two;
+            PokerCardSuit similarSuit;
+            PokerCardRank similarRank;
             int similarSuits = GetSimilarSuit(cards, out similarSuit);
             int similarRanks = GetSimilarRank(cards, out similarRank);
 
+            PokerCardSuit suitWithFlush;
+            bool straight = CheckForStraight(cards);
+            bool flush = CheckForFlush(cards, out suitWithFlush);
+
             // Automatically assuming high card, if no higher combinations are found.
-            HandCombination combination = new HandCombination(CardCombo.HighCard, similarRank);
+            HandCombination combination = new HandCombination(CardCombo.HighCard, similarRank, similarSuit);
 
             // Pair
-            if (similarSuits == 2) 
-                combination = new HandCombination(CardCombo.Pair, similarRank);
+            if (similarRanks == 1)
+                combination = new HandCombination(CardCombo.Pair, similarRank, similarSuit);
 
             // Two Pair
-            if (similarSuits == 2)
+            if (similarRanks == 1)
             {
-                PokerCardSuit secondarySuit = PokerCardSuit.Clubs;
-                int secondarySimilarSuits = GetSimilarSuit(cards, out secondarySuit, similarSuit);
-                if (secondarySimilarSuits == 2) 
-                    combination = new HandCombination(CardCombo.TwoPair, similarRank);
+                PokerCardRank secondaryRank = PokerCardRank.Two;
+                int secondarySimilarRanks = GetSimilarRank(cards, out secondaryRank, similarRank);
+                if (secondarySimilarRanks == 1)
+                    combination = new HandCombination(CardCombo.TwoPair, similarRank, similarSuit);
             };
 
             // Three of a Kind
-            if (similarSuits == 3) 
-                combination = new HandCombination(CardCombo.ThreeOfAKind, similarRank);
+            if (similarRanks == 2)
+                combination = new HandCombination(CardCombo.ThreeOfAKind, similarRank, similarSuit);
 
             // Straight
-
+            if (straight)
+                combination = new HandCombination(CardCombo.Straight, similarRank, similarSuit);
 
             // Flush
-
+            if (flush)
+                combination = new HandCombination(CardCombo.Flush, similarRank, suitWithFlush);
 
             // Full House
-            if (similarSuits == 3)
+            if (similarRanks == 3)
             {
-                PokerCardSuit secondarySuit = PokerCardSuit.Clubs;
-                int secondarySimilarSuits = GetSimilarSuit(cards, out secondarySuit, similarSuit);
-                if (secondarySimilarSuits == 2) 
-                    combination = new HandCombination(CardCombo.TwoPair, similarRank);
+                PokerCardRank secondaryRank;
+                int secondarySimilarRanks = GetSimilarRank(cards, out secondaryRank, similarRank);
+                if (secondarySimilarRanks == 2)
+                    combination = new HandCombination(CardCombo.FullHouse, new PokerCardRank[] { similarRank, secondaryRank }, similarSuit);
             };
+
             // Four of a Kind
-            if (similarSuits == 4) 
-                combination = new HandCombination(CardCombo.FourOfAKind, similarRank);
+            if (similarRanks == 4)
+                combination = new HandCombination(CardCombo.FourOfAKind, similarRank, similarSuit);
 
             // Straight Flush
 
 
+
             return combination;
+        }
+
+        public static bool CheckForFlush(List<StandardCard> cards, out PokerCardSuit suit)
+        {
+            int suitCount = GetSimilarSuit(cards, out suit);
+            return suitCount >= 4;
+        }
+
+        public static bool CheckForStraight(List<StandardCard> cards)
+        {
+            bool result = false;
+            var cardsSortedByRank = cards;
+            cardsSortedByRank.Sort((a, b) => a.rank - b.rank);
+
+            int rankCount = 0;
+            PokerCardRank lastRank = PokerCardRank.Two;
+            for (int i = 0; i < cards.Count; i++)
+            {
+                if (cardsSortedByRank[i].rank < lastRank) rankCount = 0;
+                if (cardsSortedByRank[i].rank == ++lastRank) rankCount++;
+                if (rankCount >= 4) break;
+                lastRank = cards[i].rank;
+            }
+            result = rankCount >= 4;
+
+            return result;
         }
 
         public static PokerCardRank GetHighestRank(List<StandardCard> cards)
@@ -162,13 +218,19 @@ namespace Poker
             int result = 0;
             int countingSuits = 0;
             PokerCardSuit currentSuit = PokerCardSuit.Clubs;
+            List<StandardCard> cardsSortedBySuit = cards;
+            cardsSortedBySuit.Sort((a, b) => a.suit - b.suit);
 
-            foreach (PokerCardSuit _rank in Enum.GetValues(typeof(PokerCardSuit)))
+            foreach (PokerCardSuit _suit in Enum.GetValues(typeof(PokerCardSuit)))
             {
-                currentSuit = _rank;
-                foreach (StandardCard card in cards)
+                currentSuit = _suit;
+                foreach (StandardCard card in cardsSortedBySuit)
                 {
-                    if (_rank == card.suit) countingSuits++;
+                    if (_suit == card.suit) countingSuits++;
+                    if (_suit != card.suit) { 
+                        countingSuits = 0; 
+                        currentSuit = card.suit; 
+                    }
                     if (countingSuits > result) result = countingSuits;
                 }
             }
---
+++ PokerTest/Unit/HandCombinationHandler_UnitTests.cs
@@ -0,0 +1,193 @@
+﻿using NUnit.Framework;
+using Poker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerTest.Unit
+{
+    internal class HandCombinationHandler_UnitTests
+    {
+        internal class GetHighestRank
+        {
+            [Test]
+            public void TestSimpleCombinations()
+            {
+                
+            }
+        }
+        internal class GetSimilarSuit
+        {
+            [Test]
+            public void TestSimpleCombination()
+            {
+                StandardCard[] cards_with_three_spades =
+                {
+                    new StandardCard(PokerCardSuit.Spades, PokerCardRank.Eight),
+                    new StandardCard(PokerCardSuit.Spades, PokerCardRank.Six),
+                    new StandardCard(PokerCardSuit.Hearts, PokerCardRank.Four),
+                    new StandardCard(PokerCardSuit.Diamonds, PokerCardRank.Jack),
+                    new StandardCard(PokerCardSuit.Spades, PokerCardRank.Queen),
+                    new StandardCard(PokerCardSuit.Clubs, PokerCardRank.Two),
+                    new StandardCard(PokerCardSuit.Hearts, PokerCardRank.Ace),
+                };
+                StandardCard[] cards_with_four_diamonds =
+                {
+                    new StandardCard(PokerCardSuit.Diamonds, PokerCardRank.Eight),
+                    new StandardCard(PokerCardSuit.Diamonds, PokerCardRank.Six),
+                    new StandardCard(PokerCardSuit.Diamonds, PokerCardRank.Four),
+                    new StandardCard(PokerCardSuit.Diamonds, PokerCardRank.Jack),
+                    new StandardCard(PokerCardSuit.Spades, PokerCardRank.Queen),
+                    new StandardCard(PokerCardSuit.Clubs, PokerCardRank.Two),
+                    new StandardCard(PokerCardSuit.Hearts, PokerCardRank.Ace),
+                };
+                StandardCard[] cards_with_five_hearts =
+                {
+                    // Royal-flush
+                    new StandardCard(PokerCardSuit.Hearts, PokerCardRank.Ten),
+                    new StandardCard(PokerCardSuit.Hearts, PokerCardRank.King),
+                    new StandardCard(PokerCardSuit.Hearts, PokerCardRank.Jack),
+                    new StandardCard(PokerCardSuit.Hearts, PokerCardRank.Queen),
+                    new StandardCard(PokerCardSuit.Hearts, PokerCardRank.Ace),
+                    new StandardCard(PokerCardSuit.Clubs, PokerCardRank.Two),
+                    new StandardCard(PokerCardSuit.Spades, PokerCardRank.Four),
+                };
+
+                PokerCardSuit spade;
+                PokerCardSuit diamond;
+                PokerCardSuit heart;
+
+                int spadeCount = HandCombinationHandler.GetSimilarSuit(cards_with_three_spades.ToList(), out spade);
+                int diamondCount = HandCombinationHandler.GetSimilarSuit(cards_with_four_diamonds.ToList(), out diamond);
+                int heartCount = HandCombinationHandler.GetSimilarSuit(cards_with_five_hearts.ToList(), out heart);
+
+                // counts from index 0 at the moment.
+                Assert.AreEqual(2, spadeCount, $"Expected index count of {2} spades. Got {spadeCount} with resulting suit {spade}");
+                Assert.AreEqual(3, diamondCount, $"Expected index count of {3} diamonds. Got {diamondCount} with resulting suit {diamond}");
+                // This is what the intellisense is suggesting below, if you can see the green squiggles directly above.
+                Assert.That(4, Is.EqualTo(heartCount), $"Expected index count of {4} hearts. Got {heartCount} with resulting suit {heart}");
+            }
+        }
+        internal class GetSimilarRank
+        {
+            [Test]
+            public void TestSimpleCombination()
+            {
+
+            }
+        }
+
+        internal class CheckForFlush
+        {
+            [Test]
+            public void TestSimpleCombinations()
+            {
+                StandardCard[] cards_with_a_flush =
+                {
+                    new StandardCard(PokerCardSuit.Spades, PokerCardRank.Eight),
+                    new StandardCard(PokerCardSuit.Spades, PokerCardRank.Six),
+                    new StandardCard(PokerCardSuit.Spades, PokerCardRank.Four),
+                    new StandardCard(PokerCardSuit.Spades, PokerCardRank.Jack),
+                    new StandardCard(PokerCardSuit.Spades, PokerCardRank.Queen),
+                    new StandardCard(PokerCardSuit.Clubs, PokerCardRank.Two),
+                    new StandardCard(PokerCardSuit.Hearts, PokerCardRank.Ace),
+                };
+                StandardCard[] cards_without_a_flush =
+                {
+                    new StandardCard(PokerCardSuit.Spades, PokerCardRank.Eight),
+                    new StandardCard(PokerCardSuit.Diamonds, PokerCardRank.Six),
+                    new StandardCard(PokerCardSuit.Hearts, PokerCardRank.Four),
+                    new StandardCard(PokerCardSuit.Spades, PokerCardRank.Jack),
+                    new StandardCard(PokerCardSuit.Spades, PokerCardRank.Queen),
+                    new StandardCard(PokerCardSuit.Clubs, PokerCardRank.Two),
+                    new StandardCard(PokerCardSuit.Hearts, PokerCardRank.Ace),
+                };
+
+                PokerCardSuit flushSuit;
+                PokerCardSuit nonFlushSuit;
+
+                bool flush_hand = HandCombinationHandler.CheckForFlush(cards_with_a_flush.ToList(), out flushSuit);
+                bool non_flush_hand = HandCombinationHandler.CheckForFlush(cards_without_a_flush.ToList(), out nonFlushSuit);
+
+                Assert.IsTrue(flush_hand, $"Checking for a flush, returned {flush_hand} with suit: {flushSuit}");
+                Assert.IsFalse(non_flush_hand, $"Checking for a non-flush hand, returned {non_flush_hand} with suit: {nonFlushSuit}");
+            }
+        }
+        internal class CheckForStraight
+        {
+            [Test]
+            public void TestSimpleCombinations()
+            {
+                StandardCard[] cards_with_a_straight =
+                {
+                    // The straight cards
+                    new StandardCard(PokerCardSuit.Spades, PokerCardRank.Eight),
+                    new StandardCard(PokerCardSuit.Clubs, PokerCardRank.Nine),
+                    new StandardCard(PokerCardSuit.Hearts, PokerCardRank.Ten),
+                    new StandardCard(PokerCardSuit.Diamonds, PokerCardRank.Jack),
+                    new StandardCard(PokerCardSuit.Spades, PokerCardRank.Queen),
+                    // the non-straight cards
+                    new StandardCard(PokerCardSuit.Clubs, PokerCardRank.Two),
+                    new StandardCard(PokerCardSuit.Hearts, PokerCardRank.Ace),
+                };
+                StandardCard[] cards_without_a_straight =
+                {
+                    // The non-straight cards
+                    new StandardCard(PokerCardSuit.Spades, PokerCardRank.Eight),
+                    new StandardCard(PokerCardSuit.Clubs, PokerCardRank.Six),
+                    new StandardCard(PokerCardSuit.Hearts, PokerCardRank.Four),
+                    new StandardCard(PokerCardSuit.Diamonds, PokerCardRank.Jack),
+                    new StandardCard(PokerCardSuit.Spades, PokerCardRank.Queen),
+                    new StandardCard(PokerCardSuit.Clubs, PokerCardRank.Two),
+                    new StandardCard(PokerCardSuit.Hearts, PokerCardRank.Ace),
+                };
+
+                bool straight_hand = HandCombinationHandler.CheckForStraight(cards_with_a_straight.ToList());
+                bool non_straight_hand = HandCombinationHandler.CheckForStraight(cards_without_a_straight.ToList());
+
+                Assert.IsTrue(straight_hand, $"Checking for a straight, returned {straight_hand}");
+                Assert.IsFalse(non_straight_hand, $"Checking for a not-straight hand, returned {non_straight_hand}");
+            }
+
+            [Test]
+            public void TestManyCombinations()
+            {
+                Dictionary<int, (StandardCard[], bool)> cardsWithResultsDictionary = new Dictionary<int, (StandardCard[], bool)>
+                {
+                    { 1, (new StandardCard[] 
+                        {
+                            new StandardCard(PokerCardSuit.Spades, PokerCardRank.Eight),
+                            new StandardCard(PokerCardSuit.Clubs, PokerCardRank.Six),
+                            new StandardCard(PokerCardSuit.Hearts, PokerCardRank.Four),
+                            new StandardCard(PokerCardSuit.Diamonds, PokerCardRank.Jack),
+                            new StandardCard(PokerCardSuit.Spades, PokerCardRank.Queen),
+                            new StandardCard(PokerCardSuit.Clubs, PokerCardRank.Two),
+                            new StandardCard(PokerCardSuit.Hearts, PokerCardRank.Ace),
+                        }, false 
+                    )},
+                    { 2, (new StandardCard[]
+                        {
+                            // The straight cards
+                            new StandardCard(PokerCardSuit.Spades, PokerCardRank.Eight),
+                            new StandardCard(PokerCardSuit.Clubs, PokerCardRank.Nine),
+                            new StandardCard(PokerCardSuit.Hearts, PokerCardRank.Ten),
+                            new StandardCard(PokerCardSuit.Diamonds, PokerCardRank.Jack),
+                            new StandardCard(PokerCardSuit.Spades, PokerCardRank.Queen),
+                            // the non-straight cards
+                            new StandardCard(PokerCardSuit.Clubs, PokerCardRank.Two),
+                            new StandardCard(PokerCardSuit.Hearts, PokerCardRank.Ace),
+                        }, true 
+                     )},
+                };
+
+                foreach (var cards in cardsWithResultsDictionary)
+                {
+                    bool result = HandCombinationHandler.CheckForStraight(cards.Value.Item1.ToList());
+                    Assert.IsTrue(result == cards.Value.Item2, $"Got: {result}, Expected: {cards.Value.Item2}");
+                }
+            }
+        }
+    }
+}
